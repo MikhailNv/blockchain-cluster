17c89771e081da61d568ae0139edf66e190f7252
MikhailNv <mihail20022015@gmail.com>
2024-07-20 21:02:19 +0300

    стартовая реализация блокчейна

--- blockchain/backend/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def hash256(string):
-    """Two round of sha256"""
-    first_iter = hashlib.sha256(string).digest()
-    second_iter = hashlib.sha256(first_iter).digest()
-    return second_iter
